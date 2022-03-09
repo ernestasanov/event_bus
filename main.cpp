42d7540f3332a90842deaa3d8a5353ad8520c951
ernesta <ea@neiry-bci.com>
2022-03-09 16:59:04 +0300

    comments

--- main.cpp
+++ main.cpp
@@ -27,17 +27,22 @@ using EventQueue = event_bus::EventQueue<EventBus>;
 
 int main() {
     std::atomic_bool finished = false;
+    // Event bus is common
     auto eventBus = std::make_shared<EventBus>();
     event_bus::ThreadToken worker1Token;
     auto worker = std::thread([&eventBus, &finished, &worker1Token]{
+        // Event queues are per thread
         EventQueue eventQueue { eventBus };
         worker1Token = eventQueue.GetToken();
         eventQueue.Subscribe<MyEvent::Start>(&eventQueue, [&eventBus](auto eventData) {
+            // event data is std::shared_ptr<const int>
+            // this lambda is called in worker thread 1
             auto eventParam = *eventData; // int
             std::cout << "Worker 1 start event " << eventParam << std::endl;
             eventBus->FireEvent<MyEvent::Stop>(std::make_shared<float>(5.0f));
         });
         while (!finished) {
+            // event queue callbacks are called in this loop
             eventQueue.HandleEvents();
         }
         eventQueue.Unsubscribe<MyEvent::Start>(&eventQueue); // Not required
@@ -45,6 +50,7 @@ int main() {
     auto worker2 = std::thread([&eventBus, &finished]{
         EventQueue eventQueue { eventBus };
         eventQueue.Subscribe<MyEvent::Start>(&eventQueue, [](auto eventData) {
+            // this lambda is called in worker thread 2
             auto eventParam = *eventData; // int
             std::cout << "Worker 2 start event " << eventParam << std::endl;
         });
@@ -60,6 +66,7 @@ int main() {
     });
     eventBus->Start();
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    // Post event for all subscribers to consume
     eventBus->FireEvent<MyEvent::Start>(std::make_shared<int>(1));
     std::this_thread::sleep_for(std::chrono::seconds(5));
     finished = true;
